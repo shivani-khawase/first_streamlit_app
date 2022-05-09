67755d02aaacfcef0cbed8b550d53561b159f238
shivani khawase <105192404+shivani-khawase@users.noreply.github.com>
2022-05-09 12:15:56 +0530

    Update streamlit_app.py
    fixed typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('my first file')
 streamlit.header('Menu')
 streamlit.text('otameal')
-sreamlit.text('spanich')
+streamlit.text('spanich')
 streamlit.text('half boiled egg')
